2fdd29635de3d201f8aa2bd47bacef26829dc6d9
andres <velasco.andrs@gmail.com>
2021-01-05 23:04:57 -0500

    init provider

---
+++ provider/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'pages/pagina1_page.dart';
+ 
+import 'package:singleton/pages/pagina2_page.dart';
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'pagina1',
+      routes: {
+        'pagina1': (_) => Pagina1Page(),
+        'pagina2': (_) => Pagina2Page(),
+      },
+    );
+  }
+}
---
+++ provider/lib/pages/pagina1_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Pagina1Page extends StatelessWidget {
+  const Pagina1Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina 1'),
+      ),
+      body: Container(
+        child: InformacionUsuario(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.access_alarm),
+        onPressed: () => Navigator.popAndPushNamed(context, 'pagina2'),
+      ),
+    );
+  }
+}
+
+class InformacionUsuario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'General',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Nombre: '),
+          ),
+          ListTile(
+            title: Text('Edad: '),
+          ),
+          Divider(),
+          Text(
+            'Profesiones',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Profesion 1: '),
+          ),
+          ListTile(
+            title: Text('Profesion 2: '),
+          ),
+        ],
+      ),
+    );
+  }
+}

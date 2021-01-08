1785516fc154c006b14d8ba704af9efaab30ae2e
andres <velasco.andrs@gmail.com>
2021-01-07 21:46:11 -0500

    cubit init

---
+++ cubit_example/lib/pages/pagina2_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+  const Pagina2Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina 2'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                color: Colors.blue,
+                child: Text(
+                  'Establecer Usuario',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                onPressed: () => {},
+              ),
+
+              MaterialButton(
+                color: Colors.blue,
+                child: Text(
+                  'Cambiar edad',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                onPressed: () => {},
+              ),
+
+              MaterialButton(
+                color: Colors.blue,
+                child: Text(
+                  'Añadir una profesion',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+                onPressed: () => {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
